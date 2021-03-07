606a1b69d661aac19c2fe53894341cbd771f41fc
Marshood Ayoub <marshood.ayoub@gmail.com>
2021-03-07 15:45:32 +0200

    adding free text filtering

--- agridapp/src/App.js
+++ agridapp/src/App.js
@@ -10,18 +10,15 @@ function App() {
     // console.log("data , ",json)
     setRowData(json);
     return () => {
-
     }
   }, [])
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
+  const [rowData, setRowData] = useState([]);
  
-  const [rowData, setRowData] = useState([
-
-  ]);
   const columns = [
     { headerName: "id", field: 'id', checkboxSelection: true },
-    { headerName: "BarCode", field: 'BarCode', tooltipField: "BarCode" },
+    { headerName: "BarCode", field: 'BarCode', tooltipField: "BarCode"  },
     { headerName: "ProduName", field: 'ProduName', tooltipField: "ProduName" },
     { headerName: "Segment", field: 'Segment', tooltipField: "Segment" },
     { headerName: "Brand", field: 'Brand', tooltipField: "Brand" },
@@ -29,57 +26,96 @@ function App() {
     { headerName: "TotalPrice", field: 'TotalPrice', tooltipField: "TotalPrice" },
     { headerName: "Percent_TotalPrice", field: 'Percent_TotalPrice', tooltipField: "Percent_TotalPrice", valueFormatter: PercentFormatter },
     { headerName: "TotalAmount", field: 'TotalAmount', tooltipField: "TotalAmount" },
-    { headerName: "Percent_TotalAmount", field: 'Percent_TotalAmount', tooltipField: "Percent_TotalAmount" },
+    { headerName: "Percent_TotalAmount", field: 'Percent_TotalAmount', tooltipField: "Percent_TotalAmount", valueFormatter: PercentFormatter },
     { headerName: "Percent_Avg", field: 'Percent_Avg', tooltipField: "Percent_Avg" },
     { headerName: "Percent_Profit", field: 'Percent_Profit', tooltipField: "Percent_Profit" },
-    { headerName: "Percent_MarketAmount", field: 'Percent_MarketAmount', tooltipField: "Percent_MarketAmount" },
+    { headerName: "Percent_MarketAmount", field: 'Percent_MarketAmount', tooltipField: "Percent_MarketAmount", valueFormatter: PercentFormatter },
     { headerName: "MarketPrice", field: 'MarketPrice', tooltipField: "MarketPrice" },
     { headerName: "Status", field: 'Status', tooltipField: "Status" },
     { headerName: "Remark", field: 'Remark', tooltipField: "Remark" },
-    { headerName: "date", field: 'date', tooltipField: "date" },
+    {
+      headerName: "date", field: 'date', tooltipField: "date",
+      filter: "agDateColumnFilter",
+      filterParams: {
+        debounceMs: 500,
+        suppressAndOrCondition: true,
+        comparator: function (filterLocalDateAtMidnight, cellValue) {
+          if (cellValue == null) {
+            return 0;
+          }
+          var dateParts = cellValue.split("/");
+          var year = Number(dateParts[2]);
+          var month = Number(dateParts[1]) - 1;
+          var day = Number(dateParts[0]);
+          var cellDate = new Date(year, month, day);
+          if (cellDate < filterLocalDateAtMidnight) {
+            return -1;
+          } else if (cellDate > filterLocalDateAtMidnight) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      }
+    },
 
   ]
   function PercentFormatter(params) {
     return params.value + '%';//formatNumber(params.value)
   }
 
+
+
+
+
   const defaultColDef = {
-    sortable: true, editable: true, filter: true, floatingFilter: true,
+    sortable: true,
+    // editable: true,
+    filter: true,
+    floatingFilter: true,
     flex: true,
-  };
-  const onGridReady= (params) => {
+    
+   };
+  const onGridReady = (params) => {
     setGridApi(params);
     setGridColumnApi(params.columnApi);
- 
-}
+  }
   const onFilterTextChange = (e) => {
     gridApi.api.setQuickFilter(e.target.value)
   }
+  var gridOptions = {
+    columnDefs: defaultColDef,
+    defaultColDef: {
+      sortable: true,
+    },
+    autoGroupColumnDef: {
+      comparator: function (valueA, valueB, nodeA, nodeB, isInverted) {
+        var res = valueA == valueB ? 0 : valueA > valueB ? 1 : -1;
+        return res;
+      },
+      field: 'athlete',
+      sort: 'asc',
+    },
+  };
+  
   return (
     <div className="App">
-      <div className="ag-theme-alpine center" style={{ height: 400, width: 600 }}>
+      <div className="ag-theme-alpine center" style={{ height: '600px', width: '1000px' }}>
 
-        <input type="serach" onChange={onFilterTextChange}
+        <input type="serach" onChange={onFilterTextChange} 
           placeholder="Search"></input>
         <AgGridReact
           rowData={rowData}
           defaultColDef={defaultColDef}
           columnDefs={columns}
           onGridReady={onGridReady}
-          // enableBrowserTooltips={true}
+         gridOptions={gridOptions}
+        // enableBrowserTooltips={true}
         // tooltipShowDelay={{ tooltipShowDelay: 2 }}
         // sideBar={true}
-
+    
         >
-          {/*          
-          <AgGridColumn field="id" />
-          <AgGridColumn field="name" minWidth={180} />
-          <AgGridColumn field="body" />
-          <AgGridColumn
-            field="athlete"
-            filter="agTextColumnFilter"
-            minWidth={200}
-          /> */}
+         
         </AgGridReact>
 
       </div>
